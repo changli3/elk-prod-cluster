86061b62649c82cf02841d3a2f75c2cc0ea1a41f
Chang Li <chang.li3@treasury.gov>
2018-04-04 13:40:01 -0400

    update cloundformation template

--- install-elasticsearch.sh
+++ install-elasticsearch.sh
@@ -11,7 +11,6 @@ printf 'y' | yum install -y python-pip
 
 pip install --upgrade pip
 pip install awscli
-pip install –upgrade awscli
 
 echo "[elasticsearch-6.x]
 name=elasticsearch repository for 6.x packages
@@ -22,14 +21,16 @@ enabled=1
 autorefresh=1
 type=rpm-md" | tee /etc/yum.repos.d/elasticsearch.repo
 yum -y install elasticsearch
+
+chown -R elasticsearch:elasticsearch /mnt/elasticsearch
+chown elasticsearch:elasticsearch /etc/elasticsearch/elasticsearch.yml
+printf 'yes' | /usr/share/elasticsearch/bin/elasticsearch-plugin install discovery-ec2
+service elasticsearch start
 systemctl enable elasticsearch.service
 
 cd /tmp
 curl -LO https://github.com/prometheus/node_exporter/releases/download/v0.16.0-rc.0/node_exporter-0.16.0-rc.0.linux-amd64.tar.gz
 tar xvf node_exporter-0.16.0-rc.0.linux-amd64.tar.gz
 cp node_exporter-0.16.0-rc.0.linux-amd64/node_exporter /usr/local/bin
-chown -R elasticsearch:elasticsearch /mnt/elasticsearch
-chown elasticsearch:elasticsearch /etc/elasticsearch/elasticsearch.yml
-printf 'yes' | /usr/share/elasticsearch/bin/elasticsearch-plugin install discovery-ec2
-service elasticsearch start
+
 /usr/local/bin/node_exporter &
