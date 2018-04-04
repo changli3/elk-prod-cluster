9171752635d0fd1c6e78223aec590d2ef9a8c693
Chang Li <chang.li3@treasury.gov>
2018-04-04 19:36:36 -0400

    update cf template

--- install-kibana.sh
+++ install-kibana.sh
@@ -1,7 +1,8 @@
 mkfs -t ext4 /dev/xvdcy
-mkdir /mnt/elasticsearch
-mount /dev/xvdcy /mnt/elasticsearch
-echo "/dev/xvdcy       /mnt/elasticsearch   ext4    defaults,nofail        0       2" | tee -a /etc/fstab
+mkdir -p /mnt/elasticsearch
+mkdir -p /mnt/kibana
+mount /dev/xvdcy /mnt/kibana
+echo "/dev/xvdcy       /mnt/kibana   ext4    defaults,nofail        0       2" | tee -a /etc/fstab
 
 cd /tmp
 wget http://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
@@ -38,15 +39,44 @@ discovery.zen.hosts_provider: ec2
 discovery.ec2.groups: $2
 discovery.ec2.host_type: private_ip
 network.publish_host: _eth0:ipv4_
+node.master: false
+node.data: false
+node.ingest: false
 " | tee -a /etc/elasticsearch/elasticsearch.yml
 
 chown -R elasticsearch:elasticsearch /mnt/elasticsearch
 chown elasticsearch:elasticsearch /etc/elasticsearch/elasticsearch.yml
-
 printf 'yes' | /usr/share/elasticsearch/bin/elasticsearch-plugin install discovery-ec2
-
-service elasticsearch start
 systemctl enable elasticsearch.service
+sudo service elasticsearch restart
+
+
+echo "[kibana-6.x]
+name=Kibana repository for 6.x packages
+baseurl=https://artifacts.elastic.co/packages/6.x/yum
+gpgcheck=1
+gpgkey=https://artifacts.elastic.co/GPG-KEY-elasticsearch
+enabled=1
+autorefresh=1
+type=rpm-md" | tee /etc/yum.repos.d/Kibana.repo
+
+yum -y install kibana
+chkconfig --add kibana
+/sbin/chkconfig kibana on
+
+echo "
+server.host: 0.0.0.0
+server.defaultRoute: /app/kibana
+elasticsearch.url: http://localhost:9200
+kibana.index: .index
+kibana.defaultAppId: discover
+path.data: /mnt/kibana
+status.allowAnonymous: true
+" | sudo tee -a /etc/kibana/kibana.yml
+
+chown -R kibana:kibana /mnt/kibana
+chown kibana:kibana /etc/kibana/kibana.yml
+sudo service kibana restart
 
 cd /tmp
 curl -LO https://github.com/prometheus/node_exporter/releases/download/v0.16.0-rc.0/node_exporter-0.16.0-rc.0.linux-amd64.tar.gz
@@ -54,4 +84,3 @@ tar xvf node_exporter-0.16.0-rc.0.linux-amd64.tar.gz
 cp -f node_exporter-0.16.0-rc.0.linux-amd64/node_exporter /usr/local/bin
 
 /usr/local/bin/node_exporter &
-service elasticsearch start
