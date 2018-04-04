9744b8c6a653990d0b888acfe55859802314d64b
Chang Li <chang.li3@treasury.gov>
2018-04-04 14:57:49 -0400

    updated cf template

--- install-elasticsearch.sh
+++ install-elasticsearch.sh
@@ -22,15 +22,29 @@ autorefresh=1
 type=rpm-md" | tee /etc/yum.repos.d/elasticsearch.repo
 yum -y install elasticsearch
 
+
+sed -i 's/path.data:/#path.data:/g' /etc/elasticsearch/elasticsearch.yml
+echo "
+cluster.name: $1
+path.data: /mnt/elasticsearch
+network.bind_host: 0.0.0.0
+discovery.zen.minimum_master_nodes: 2
+discovery.zen.hosts_provider: ec2
+discovery.ec2.groups: $2
+discovery.ec2.host_type: private_ip
+" | tee -a /etc/elasticsearch/elasticsearch.yml
+
 chown -R elasticsearch:elasticsearch /mnt/elasticsearch
 chown elasticsearch:elasticsearch /etc/elasticsearch/elasticsearch.yml
+
 printf 'yes' | /usr/share/elasticsearch/bin/elasticsearch-plugin install discovery-ec2
+
 service elasticsearch start
 systemctl enable elasticsearch.service
 
 cd /tmp
 curl -LO https://github.com/prometheus/node_exporter/releases/download/v0.16.0-rc.0/node_exporter-0.16.0-rc.0.linux-amd64.tar.gz
 tar xvf node_exporter-0.16.0-rc.0.linux-amd64.tar.gz
-cp node_exporter-0.16.0-rc.0.linux-amd64/node_exporter /usr/local/bin
+cp -f node_exporter-0.16.0-rc.0.linux-amd64/node_exporter /usr/local/bin
 
 /usr/local/bin/node_exporter &
